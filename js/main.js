28b2909e4ed75bc0137c0c03685f30e58785498d
Салмин Рудольф <rsalmin@sdvor.local>
2021-09-28 23:49:09 +0500

    fix #2 add second function

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
-function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
+function randomNumber(min,max) {
+  if(min < max || min >= 0) {
+     return Math.floor(Math.random() * (max - min + 1)) + min;
+  } else{
+    console.log('значения минимум и максимум не корректны')
   }
+randomNumber(20,70);
 // взято с https://developer.mozilla.org/
-
-
-
+function commentLength(strng,maxLength) {
+  if (strng.length >= maxLength) {
+    return false;
+  } else{
+    return true;
+  }
+};
+commentLength('string',140);
+// взято с https://www.cyberforum.ru/javascript/thread1165903.html
