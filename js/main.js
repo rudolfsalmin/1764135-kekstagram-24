fe075572a9e43973f87b2afc69eaf65d3ab986a3
Салмин Рудольф <rsalmin@sdvor.local>
2021-09-30 22:27:16 +0500

    fix #2 поменял

--- js/main.js
+++ js/main.js
@@ -1,17 +1,15 @@
-function randomNumber(min,max) {
-  if(min < max || min >= 0) {
-     return Math.floor(Math.random() * (max - min + 1)) + min;
-  } else{
-    console.log('значения минимум и максимум не корректны')
-  }
-randomNumber(20,70);
+const randomNumber = (min,max) => {
+  if (min < max || min >= 0)
+  {return Math.floor(Math.random() * (max - min + 1)) + min;}
+};
+randomNumber(-30,50);
 // взято с https://developer.mozilla.org/
-function commentLength(strng,maxLength) {
+const commentLength = (strng,maxLength) => {
   if (strng.length >= maxLength) {
     return false;
-  } else{
-    return true;
   }
+  return true;
 };
 commentLength('string',140);
+
 // взято с https://www.cyberforum.ru/javascript/thread1165903.html
