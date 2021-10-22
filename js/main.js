8d4b74d77fbe66566b85580ebdf5a6e4523123ce
Салмин Рудольф <rsalmin@sdvor.local>
2021-10-22 15:42:09 +0500

    add 25 objects

--- js/main.js
+++ js/main.js
@@ -1,16 +1,2 @@
-const getRandomNumber = (min,max) => {
-  if (min <0 && max<0) {
-    return undefined;
-  }
-  const low = min>0 ? min: 0;
-  const random = Math.random();
-  const res = low + Math.round(random* (max-low));
-  return res;
-};
-
-getRandomNumber(-10, -60);
-// взято с https://developer.mozilla.org/
-const getCommentLength = (strng,maxLength) => strng.length <= maxLength;
-getCommentLength('string',140);
-
-// взято с https://www.cyberforum.ru/javascript/thread1165903.html
+import './utils.js'
+import './data.js'
