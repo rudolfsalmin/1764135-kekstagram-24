a1a5272935b889e4ea75cc73a60a815aa4889a45
Салмин Рудольф <rsalmin@sdvor.local>
2021-11-03 14:08:13 +0500

    fix #2

--- js/picture.js
+++ js/picture.js
@@ -3,12 +3,13 @@ import { getRandomNumber, isEnterKey } from "./utils.js";
 const userPictureItem = photos();
 const picturesContainer = document.querySelector('.pictures');
 const picturesTemplate = document.querySelector('#picture').content.querySelector('.picture');
+const pictureFragment = document.createDocumentFragment();
 userPictureItem.forEach(({ url, like, comments}) =>{
   pictureElement = picturesTemplate.cloneNode(true);
   pictureElement.querySelector('.picture__img').src = url;
   pictureElement.querySelector('.picture__likes').textContent = like.toString();
   pictureElement.querySelector('.picture__comments').textContent = comments.length.toString();
-  picturesContainer.appendChild(pictureElement);
+  pictureFragment.appendChild(pictureElement);
 });
-
+picturesContainer.appendChild(pictureFragment);
 export {userPictureItem};
